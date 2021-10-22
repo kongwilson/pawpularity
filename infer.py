4a47d230b7fabcd8497ff024c4def536d3502d94
Weicong Kong <weicong.kong@gmail.com>
2021-10-22 19:34:07 +1100

    add infer function

--- infer.py
+++ infer.py
@@ -3,14 +3,8 @@ To use the model trained on test dataset
 
 Copyright (C) Weicong Kong, 16/10/2021
 """
-import glob
 
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-
-from loader import PawPreprocessor, PawDataset
-from model import *
-from utilities import *
+from train import *
 
 
 def infer(model_type, img_size=384, batch_size=4, embed_size=128, hidden_size=64):
@@ -53,6 +47,54 @@ def infer(model_type, img_size=384, batch_size=4, embed_size=128, hidden_size=64
 					temp_preds = predictions
 				else:
 					temp_preds = np.vstack((temp_preds, predictions))
+
+		if preds is None:
+			preds = temp_preds
+		else:
+			preds += temp_preds
+
+	preds /= (len(all_models_checkpoints))
+	return preds
+
+
+def infer_with_xgb(model_type, img_size=384, batch_size=4, embed_size=128, hidden_size=64):
+
+	seed_everything()
+	device = get_default_device()
+	preprocessor = PawPreprocessor(root_dir=data_root, train=False)
+	test_img_paths, test_dense, test_targets = preprocessor.get_data()
+
+	test_dataset = PawDataset(
+		images_filepaths=test_img_paths,
+		dense_features=test_dense,
+		targets=test_targets,
+		transform=get_albumentation_transform_for_validation(img_size)
+	)
+
+	all_models_checkpoints = glob.glob(model_root + os.path.sep + f'{model_type.__name__}_*.pth.tar')
+	preds = None
+	for model_path in all_models_checkpoints:
+
+		model_name = os.path.basename(model_path).split(os.path.extsep)[:-1]
+		xgb_path = [p for p in all_models_checkpoints if model_name in p and '.json' in p][0]
+
+		model = model_type(3, len(preprocessor.features), embed_size, hidden_size)
+		model.load_state_dict(torch.load(model_path))
+		model = model.to(device)
+		model.eval()
+
+		test_loader = DataLoader(
+			test_dataset, batch_size=batch_size,
+			shuffle=False, num_workers=0,
+			pin_memory=True
+		)
+
+		xgb_test_x, target, dl_preds = extra_intermediate_outputs_and_targets(model, test_loader)
+		xgb_model = xgb.XGBRegressor()
+		xgb_model.load_model(xgb_path)
+		xgb_preds = xgb_model.predict(xgb_test_x)
+
+		temp_preds = (xgb_preds + dl_preds) / 2
 
 		if preds is None:
 			preds = temp_preds
