38162efddd21158e11c6ea20474d32355c707462
Weicong Kong <weicong.kong@gmail.com>
2021-11-02 10:00:37 +1100

    update the infer module

--- infer.py
+++ infer.py
@@ -3,8 +3,52 @@ To use the model trained on test dataset
 
 Copyright (C) Weicong Kong, 16/10/2021
 """
+from learner import *
+from utilities import *
+from loader import *
+import glob
 
-from train import *
+activation = {}
+
+
+def get_activation(name):
+	def hook(model, input, output):
+		activation[name] = output.cpu().detach().numpy()
+
+	return hook
+
+
+def get_all_model_checkpoints(model):
+
+	all_models_checkpoints = glob.glob(model_root + os.path.sep + f'{str(model)}_*.pth.tar')
+	return all_models_checkpoints
+
+
+def extract_intermediate_outputs_and_targets(model, data_loader):
+	device = get_default_device()
+	new_x = None
+	y = None
+	preds = None
+
+	model.eval()
+
+	with torch.no_grad():
+		for (images, dense, target) in tqdm(data_loader, desc=f'Training with XGB. '):
+			images = images.to(device, non_blocking=True)
+			dense = dense.to(device, non_blocking=True)
+			batch_preds = torch.sigmoid(model(images, dense)).cpu().detach().numpy() * 100
+			batch_embed = activation['swin_head']
+			batch_x = np.concatenate([batch_embed, dense.cpu().detach().numpy()], axis=1)
+			if preds is None:
+				preds = batch_preds
+				new_x = batch_x
+				y = target.view(-1, 1).cpu().detach().numpy()
+			else:
+				preds = np.vstack((preds, batch_preds))
+				new_x = np.vstack((new_x, batch_x))
+				y = np.vstack((y, target.view(-1, 1).cpu().detach().numpy()))
+
+	return new_x, y * 100, preds
 
 
 def infer(model_type, img_size=384, batch_size=4, embed_size=128, hidden_size=64):
@@ -12,17 +56,10 @@ def infer(model_type, img_size=384, batch_size=4, embed_size=128, hidden_size=64
 	seed_everything()
 	device = get_default_device()
 	preprocessor = PawPreprocessor(root_dir=data_root, train=False)
-	test_img_paths, test_dense, test_targets = preprocessor.get_data()
-
-	test_dataset = PawDataset(
-		images_filepaths=test_img_paths,
-		dense_features=test_dense,
-		targets=test_targets,
-		transform=get_albumentation_transform_for_validation(img_size)
-	)
+	test_loader = preprocessor.get_dataloader()
 	model = model_type(3, len(preprocessor.features), embed_size, hidden_size, pretrained=False)
 
-	all_models_checkpoints = glob.glob(model_root + os.path.sep + f'{str(model)}_*.pth.tar')
+	all_models_checkpoints = get_all_model_checkpoints(model)
 	preds = None
 	for model_path in all_models_checkpoints:
 
@@ -32,18 +69,12 @@ def infer(model_type, img_size=384, batch_size=4, embed_size=128, hidden_size=64
 		model = model.to(device)
 		model.eval()
 
-		test_loader = DataLoader(
-			test_dataset, batch_size=batch_size,
-			shuffle=False, num_workers=0,
-			pin_memory=True
-		)
-
 		temp_preds = None
 		with torch.no_grad():
 			for (images, dense, target) in tqdm(test_loader, desc=f'Predicting. '):
 				images = images.to(device, non_blocking=True)
 				dense = dense.to(device, non_blocking=True)
-				predictions = torch.sigmoid(model(images, dense)).cpu().detach().numpy() * 100
+				predictions = torch.sigmoid(model(images, dense)).detach().cpu().numpy() * 100
 
 				if temp_preds is None:
 					temp_preds = predictions
@@ -64,16 +95,10 @@ def infer_with_xgb(model_type, img_size=384, batch_size=4, embed_size=128, hidde
 	seed_everything()
 	device = get_default_device()
 	preprocessor = PawPreprocessor(root_dir=data_root, train=False)
-	test_img_paths, test_dense, test_targets = preprocessor.get_data()
+	test_loader = preprocessor.get_dataloader()
 
-	test_dataset = PawDataset(
-		images_filepaths=test_img_paths,
-		dense_features=test_dense,
-		targets=test_targets,
-		transform=get_albumentation_transform_for_validation(img_size)
-	)
 	model = model_type(3, len(preprocessor.features), embed_size, hidden_size, pretrained=False)
-	all_models_checkpoints = glob.glob(model_root + os.path.sep + f'{str(model)}_*.pth.tar')
+	all_models_checkpoints = get_all_model_checkpoints(model)
 	preds = None
 	for model_path in all_models_checkpoints:
 
@@ -85,12 +110,6 @@ def infer_with_xgb(model_type, img_size=384, batch_size=4, embed_size=128, hidde
 		model.load_state_dict(torch.load(model_path))
 		model.model.head.register_forward_hook(get_activation('swin_head'))
 		model = model.to(device)
-
-		test_loader = DataLoader(
-			test_dataset, batch_size=batch_size,
-			shuffle=False, num_workers=0,
-			pin_memory=True
-		)
 
 		xgb_test_x, target, dl_preds = extract_intermediate_outputs_and_targets(model, test_loader)
 		xgb_model = xgb.XGBRegressor()
@@ -119,15 +138,10 @@ def infer_out_of_fold(model_type, img_size=384, batch_size=4, embed_size=128, hi
 	preprocessor = PawPreprocessor(root_dir=data_root, train=True, model_dir=model_root)
 	valid_img_paths, valid_dense, valid_targets = preprocessor.get_data(fold=fold, for_validation=True)
 
-	test_dataset = PawDataset(
-		images_filepaths=valid_img_paths,
-		dense_features=valid_dense,
-		targets=valid_targets,
-		transform=get_albumentation_transform_for_validation(img_size)
-	)
+	test_loader = preprocessor.get_dataloader(fold=fold, for_validation=True)
 	model = model_type(3, len(preprocessor.features), embed_size, hidden_size, pretrained=False)
 
-	all_models_checkpoints = glob.glob(model_root + os.path.sep + f'{str(model)}_*.pth.tar')
+	all_models_checkpoints = get_all_model_checkpoints(model)
 	preds = None
 	for model_path in all_models_checkpoints:
 
@@ -135,12 +149,6 @@ def infer_out_of_fold(model_type, img_size=384, batch_size=4, embed_size=128, hi
 		model.load_state_dict(torch.load(model_path))
 		model = model.to(device)
 		model.eval()
-
-		test_loader = DataLoader(
-			test_dataset, batch_size=batch_size,
-			shuffle=False, num_workers=0,
-			pin_memory=True
-		)
 
 		temp_preds = None
 		with torch.no_grad():
